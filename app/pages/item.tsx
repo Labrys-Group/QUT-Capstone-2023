1edb85d27ea89e8f10bf2b872a2343d0f521fd24
ruoxuan-goo <ruoxuan17@gmail.com>
2023-05-02 20:12:50 +1000

    code slightly clean up for review

--- app/pages/item.tsx
+++ app/pages/item.tsx
@@ -1,89 +1,95 @@
-import AddressBar from '@/components/AddressBar'
-import KeyGranted from '@/components/KeyGranted'
-import NavBar from '@/components/NavBar'
-import TitleAndDescription from '@/components/TitleAndDescription'
-import { WalletContext } from '@/context/walletContext'
-import getTotalSupply from '@/helpers/getTotalSupply'
-import { Flex, Box, Button } from '@chakra-ui/react'
-import { useSession } from 'next-auth/react'
-import { useRouter } from 'next/router'
-import { useState, useEffect, useContext } from 'react'
-import { useAccount } from 'wagmi'
-import LoadingPage from '@/components/LoadingPage'
-import NavigationButton from '@/components/NavigationButton'
+import AddressBar from "@/components/AddressBar";
+import KeyGranted from "@/components/KeyGranted";
+import NavBar from "@/components/NavBar";
+import TitleAndDescription from "@/components/TitleAndDescription";
+import { WalletContext } from "@/context/walletContext";
+import getTotalSupply from "@/helpers/getTotalSupply";
+import { Flex, Box, Button } from "@chakra-ui/react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useState, useEffect, useContext } from "react";
+import { useAccount } from "wagmi";
+import LoadingPage from "@/components/LoadingPage";
+import NavigationButton from "@/components/NavigationButton";
 
 function Item() {
-  const { address, isConnected } = useAccount()
-  const [loading, setLoading] = useState(true)
-  const [remainingToken, setRemainingToken] = useState()
-  const [tokenId, setTokenID] = useState<number | undefined>()
-  const { data: session } = useSession()
-  const router = useRouter()
+  const { address, isConnected } = useAccount();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [remainingToken, setRemainingToken] = useState<number | undefined>();
+  const [tokenId, setTokenID] = useState<number | undefined>();
+  const [access, setAccess] = useState<boolean>(false);
 
-  //verify access
-  const { erc721, accountAddress, balance } = useContext(WalletContext)
+  const { data: session } = useSession();
+  const router = useRouter();
 
-  const [access, setAccess] = useState(false)
+  // wallet context
+  const { erc721, accountAddress, balance } = useContext(WalletContext);
 
+  // get total supply
   async function getToken() {
     if (erc721 != undefined) {
-      setRemainingToken(await getTotalSupply(erc721))
+      setRemainingToken(await getTotalSupply(erc721));
     }
   }
 
+  // verify token
+  // @TODO: move this to backend and verify by erc721.balanceOf >0
   useEffect(() => {
-    console.log('run verify useEffect')
+    console.log("run verify useEffect");
+    getToken();
     // if user do not have any nfts, break
-    console.log(erc721)
-    getToken()
-    if (balance === 0 || balance === undefined) return // else
-    ;(async () => {
+    if (balance === 0 || balance === undefined) return; // else
+    (async () => {
       if (erc721 !== undefined) {
         // get number of minted nft
-        const totalSupply = await getTotalSupply(erc721)
-        console.log('totalSupply', totalSupply)
+        const totalSupply = await getTotalSupply(erc721);
+        console.log("totalSupply", totalSupply);
         if (totalSupply >= 0) {
           for (let i = 1; i <= totalSupply; i++) {
-            console.log('tokenId', i)
-            let access = await erc721.verifyAccess(accountAddress, i)
+            console.log("tokenId", i);
+            let access = await erc721.verifyAccess(accountAddress, i);
             if (access) {
-              setTokenID(i)
-              setAccess(true)
-              break
+              setTokenID(i);
+              setAccess(true);
+              break;
             }
           }
         }
       }
-    })()
-  }, [balance, erc721])
+    })();
+  }, [balance, erc721]);
 
+  // verify session
   useEffect(() => {
     const securePage = async () => {
       if (!session) {
-        router.push('/')
+        router.push("/");
       } else {
-        console.log(session)
-        setLoading(false)
+        console.log(session);
+        setLoading(false);
       }
-    }
-    securePage()
-  }, [session])
+    };
+    securePage();
+  }, [session]);
 
   if (loading) {
     return (
       <Box>
         <LoadingPage />
       </Box>
-    )
+    );
   }
 
   // @TODO: move this to constant folder
   const exy = {
-    title: 'Exy United',
+    title: "Exy United",
     description:
       "An exclusive online community dedicated to fans of EXY, leader of the K-pop girl group COSMIC GIRLS (also known as WJSN). This community is a gathering place for fans who admire EXY's talents, personality, and unique style. As a member of the EXY Community, you will have access to exclusive content, such as behind-the-scenes footage, photos, and interviews. You will also be able to connect with other fans from all over the world who share your love for EXY and COSMIC GIRLS.",
-    image: '/exyGranted.png',
-  }
+    image: "/exyGranted.png",
+  };
+
+  let displayRemainingToken =
+    remainingToken !== undefined ? 200 - remainingToken : 9999;
 
   return (
     <Box h="100vh" className="exyImg">
@@ -94,23 +100,24 @@ function Item() {
         className="pagePadding"
         marginTop="20vh"
         justifyContent="space-between"
-        alignItems="center">
+        alignItems="center"
+      >
+        <NavigationButton direction="left" goto="item-table-tennis" />
         <TitleAndDescription title={exy.title} description={exy.description} />
 
         <KeyGranted
           accessGranted={access}
-          clubName={'Exy United'}
+          clubName={"Exy United"}
           image={exy.image}
           price={0.01}
-          remainingToken={200 - remainingToken}
+          remainingToken={displayRemainingToken}
           totalToken={200}
           tokenNumber={tokenId}
         />
-        {/* <LoadingModal /> */}
         <NavigationButton direction="right" goto="item-climbing-gym" />
       </Flex>
     </Box>
-  )
+  );
 }
 
-export default Item
+export default Item;
