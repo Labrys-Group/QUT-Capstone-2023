02fbc59567dc6f94d7649a5a2af60546c14b2ef9
Anshul <n9960562@qut.edu.au>
2023-05-16 14:50:19 +1000

    spotify is back baby

--- app/pages/exy.tsx
+++ app/pages/exy.tsx
@@ -1,18 +1,20 @@
 import { getSession, useSession } from 'next-auth/react'
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
-import { Box } from '@chakra-ui/react'
+import { Box, Divider, Flex, Spacer } from "@chakra-ui/react";
+import { Grid, GridItem } from '@chakra-ui/react'
 import AddressBar from '../components/AddressBar'
 import { useAccount } from 'wagmi'
 import ContentTitle from '@/components/ContentTitle'
 import NavBar from '@/components/NavBar'
 import LoadingPage from '@/components/LoadingPage'
+import { Spotify } from "react-spotify-embed";
 
 const images = [
-  { src: 'exy_1.jpg', alt: 'Exy Lift Selfie' },
+  { src: "exy_1.jpg", alt: "Exy Peace Pose" },
   { src: 'exyLift.jpg', alt: 'Exy Lift Selfie' },
-  { src: 'exy_2.jpg', alt: 'Exy Lift Selfie' },
-  { src: 'exy_3.jpg', alt: 'Exy Lift Selfie' },
+  { src: "exy_2.jpg", alt: "Exy Black & White Pose" },
+  { src: "exy_3.jpg", alt: "Exy Showing Off Her Tteokbokki Pose" },
 ]
 
 const videos = [
@@ -21,6 +23,14 @@ const videos = [
   { src: 'https://www.youtube.com/embed/ZBPQCKpoe6c' },
   { src: 'https://www.youtube.com/embed/aWX8_QER_qQ' },
 ]
+
+const playlists = [
+  { link: "https://open.spotify.com/playlist/2FoneT7G5hqPzZHCApiZHi" },
+  { link: "https://open.spotify.com/playlist/3bnJMGcWgAI6SOg5RrJ4ao" },
+  { link: "https://open.spotify.com/playlist/3dGsgWV86aPN9wjcbkwDMf" },
+  { link: "https://open.spotify.com/playlist/0kx5D2fqVJ4JMeRBItZJ0n" },
+];
+
 function Exy() {
   const { address } = useAccount()
   const [loading, setLoading] = useState(true)
@@ -63,6 +73,7 @@ function Exy() {
           description="Members only content"
           images={images}
           videos={videos}
+          playlists={playlists}
         />
       </Box>
     </Box>
